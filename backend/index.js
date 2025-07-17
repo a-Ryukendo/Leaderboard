afe74cd2aca5564f7bcc3b7549126b2678e81bf4
a-Ryukendo <130548884+a-Ryukendo@users.noreply.github.com>
2025-07-17 20:41:29 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log('MongoDB connected')
 }).catch(e => {
   console.error('MongoDB connection error:', e)
